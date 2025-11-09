a38d06445fcf65de2161900d5375e34588dd749e
Sebastien_Sq <sebastien.escudero@gmail.com>
2025-11-09 11:11:01 +0100

    feat: ajout du composant AccountProfile pour afficher les informations des comptes

---
+++ app/components/AccountProfile.tsx
@@ -0,0 +1,15 @@
+export default function AccountProfile({title, amount, description}: {title: string, amount: string, description: string}){
+
+    return(
+        <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">{title}</h3>
+          <p className="account-amount">${amount}</p>
+          <p className="account-amount-description">{description}</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+    )
+}
--- app/pages/Profile.tsx
+++ app/pages/Profile.tsx
@@ -6,6 +6,7 @@ import {
 } from "~/hooks/useUserProfile";
 import { isValid } from "~/utils/validateForm";
 import { useUpdateUserProfileMutation } from "~/services/userApi";
+import AccountProfile from "~/components/AccountProfile";
 
 export default function Profile() {
 
@@ -117,36 +118,9 @@ export default function Profile() {
         </form>
       )}
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <AccountProfile title="Argent Bank Checking (x8349)" amount="2082.79" description="Available Balance" />
+      <AccountProfile title="Argent Bank Savings (x6712)" amount="10928.42" description="Available Balance" />
+      <AccountProfile title="Argent Bank Credit Card (x8349)" amount="184.30" description="Current Balance" />
     </div>
   );
 }
